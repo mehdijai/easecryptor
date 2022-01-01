52b2b0e055deb6fee3f3622201ae1b9085bae13b
= <=>
2022-01-01 18:53:54 +0100

    Created the functions and the documentation

--- README.md
+++ README.md
@@ -1,3 +1,107 @@
 # Easecrypto package
 
 A package to help you encrypt/decrypt files and strings with simple functions
+
+## How to use
+
+import the functions you need
+
+<code>
+    const {fileEncryptor, fileDecryptor, keyEncryptor, keyDecryptor} = require('easecrypto')
+</code>
+
+## Functions
+
+### File Encryptor
+
+#### Example
+<code>
+    fileEncryptor('./src.json', './dist.ms', privateKey,'r', ((data, err) => {
+        if(err){
+            console.error(err);
+        }else{
+            console.log(data);
+        }
+    }))
+</code>
+
+#### Functions parameters
+
+<ul>
+    <li><strong>source path</strong> the path of the file to encrypt</li>
+    <li><strong>destination path</strong> the path, name and extension of the created file</li>
+    <li><strong>encryption key</strong> private key to encrypt the file and decrypt it</li>
+    <li><strong>flag</strong> <em>null or 'r'</em> If the destination file already exists, the encryption will stop and returns an error. However, if you wont to overwrite it pass 'r' into the parameter.</li>
+    <li><strong>callback</strong> this function is asynchronous. the callback contains err and success message to make sure the process went fine.</li>
+</ul>
+
+#### Troubleshooting
+
+make sure you pass the flag parameter! pass 'r' or null. Otherwise it will throw an error.
+
+<code>
+    fileEncryptor('./src.json', './dist.ms', privateKey,null, ((data, err) => {
+        if(err){
+            console.error(err);
+        }else{
+            console.log(data);
+        }
+    }))
+</code>
+
+
+### File Decryptor
+
+#### Example
+<code>
+    fileDecryptor('./dist.ms', './new.json', privateKey, (res => {
+        console.log(res);
+    }))
+</code>
+
+#### Functions parameters
+
+<ul>
+    <li><strong>source path</strong> the path of the file to decrypt</li>
+    <li><strong>destination path</strong> <em>null or path</em> the path, name and extension of the created file. if the value is null. the callback res will return the content without writing a file to include the data.</li>
+    <li><strong>encryption key</strong> private key to encrypt the file and decrypt it</li>
+    <li><strong>callback</strong> this function is asynchronous. the callback contains a success message or the content of the file if destination file is set to null</li>
+</ul>
+
+#### Troubleshooting
+
+make sure you pass the destination path parameter! either a path value or null. Otherwise it will throw an error.
+
+<code>
+    fileDecryptor('./dist.ms', null, privateKey, (res => {
+        console.log(res);
+    }))
+</code>
+
+### Key Encryptor
+
+#### Example
+<code>
+    const encryptedKey = keyEncryptor("secret password", privateKey)
+</code>
+
+#### Functions parameters
+
+<ul>
+    <li><strong>key</strong> the string value to encrypt</li>
+    <li><strong>encryption key</strong> private key to encrypt the text and decrypt it</li>
+</ul>
+
+### Key Decryptor
+
+#### Example
+<code>
+    const decryptedKey = keyDecryptor(encryptedKey, privateKey)
+</code>
+
+#### Functions parameters
+
+<ul>
+    <li><strong>encrypted key</strong> the encrypted string value to decrypt</li>
+    <li><strong>encryption key</strong> private key to encrypt the text and decrypt it</li>
+</ul>
---
+++ index.js
@@ -0,0 +1,134 @@
+const node_crypto = require("crypto");
+const crypto_js = require("crypto-js");
+const fs = require("fs");
+
+const fileContent_encrypter = (fileContent, privateKey) => {
+  const algo = "aes-256-ctr";
+  const secretKey = node_crypto
+    .createHash("sha256")
+    .update(String(privateKey))
+    .digest("base64")
+    .substring(0, 32);
+
+  var cipher, result, iv;
+  iv = node_crypto.randomBytes(16);
+  cipher = node_crypto.createCipheriv(algo, secretKey, iv);
+  result = Buffer.concat([iv, cipher.update(fileContent), cipher.final()]);
+  return result;
+};
+
+const fileContent_decrypter = (encryptedFileContent, privateKey) => {
+  const algo = "aes-256-ctr";
+  const secretKey = node_crypto
+    .createHash("sha256")
+    .update(String(privateKey))
+    .digest("base64")
+    .substring(0, 32);
+  var decipher, result, iv;
+
+  iv = encryptedFileContent.slice(0, 16);
+
+  encryptedFileContent = encryptedFileContent.slice(16);
+
+  decipher = node_crypto.createDecipheriv(algo, secretKey, iv);
+  result = Buffer.concat([
+    decipher.update(encryptedFileContent),
+    decipher.final(),
+  ]);
+
+  return result;
+};
+
+const fileEncryptor = (sourceFile, destinationFile, privateKey, flag=null, callback) => {
+
+  if(fs.existsSync(destinationFile) && flag != 'r'){
+    callback(null, "the file already exists! if you want to overwrite it pass 'r' into flag parameter.")
+    return
+  }
+
+  fs.readFile(sourceFile, (err, file) => {
+    if (err) {
+      callback(null, err.message)
+      return
+    };
+
+    const encryptedFileContent = fileContent_encrypter(file, privateKey);
+
+    fs.writeFile(destinationFile, encryptedFileContent, (err, file) => {
+      if (err) {
+        callback(null, err.message)
+        return
+      };
+
+      callback("Encrypted Successfully!", null);
+      return
+    });
+  });
+};
+
+const fileDecryptor = (filePath, destinationPath=null, privateKey, callback) => {
+
+  if(!fs.existsSync(filePath)){
+    callback(null, "No such a file in this giver path: " + filePath)
+    return
+  }
+
+  fs.readFile(filePath, (err, file) => {
+    if (err) {
+      callback(null, err.message)
+      return
+    };
+    let decryptedFileContent = fileContent_decrypter(file, privateKey);
+    
+
+    if(destinationPath){
+
+      let file = filePath.split('.')
+      let extension = file[1]
+
+      if(extension.toLowerCase() === 'json'){
+        decryptedFileContent = JSON.stringify(decryptedFileContent, null, 2)
+      }
+
+      fs.writeFile(destinationPath, decryptedFileContent, (err, file) => {
+        if (err) {
+          callback(null, err.message)
+          return
+        };
+  
+        callback("Decrypted Successfully!", null);
+        return
+      });
+
+    }else{
+      callback(decryptedFileContent.toString('utf-8'), null);
+      return
+    }
+
+  });
+};
+
+const keyEncryptor = (key, privateKey) => {
+  const secretKey = node_crypto
+    .createHash("sha256")
+    .update(String(privateKey))
+    .digest("base64")
+    .substring(0, 32);
+  return crypto_js.AES.encrypt(key, secretKey).toString();
+};
+const keyDecryptor = (encrypted, privateKey) => {
+  const secretKey = node_crypto
+    .createHash("sha256")
+    .update(String(privateKey))
+    .digest("base64")
+    .substring(0, 32);
+  var bytes = crypto_js.AES.decrypt(encrypted, secretKey);
+  return bytes.toString(crypto_js.enc.Utf8);
+};
+
+module.exports = {
+  fileEncryptor,
+  fileDecryptor,
+  keyEncryptor,
+  keyDecryptor,
+};
