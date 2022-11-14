55333ab4efc3e750e6f85d9d479332686c9aed72
= <=>
2022-11-14 18:20:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const node_crypto = require("crypto");
-const crypto_js = require("crypto-js");
+const CryptoJS = require("crypto-js");
 const fs = require("fs");
+const Base64 = require("crypto-js/enc-base64");
 
 const fileContent_encrypter = (fileContent, privateKey) => {
   const algo = "aes-256-ctr";
@@ -31,80 +32,74 @@ const fileContent_decrypter = (encryptedFileContent, privateKey) => {
   encryptedFileContent = encryptedFileContent.slice(16);
 
   decipher = node_crypto.createDecipheriv(algo, secretKey, iv);
-  result = Buffer.concat([
-    decipher.update(encryptedFileContent),
-    decipher.final(),
-  ]);
+  result = Buffer.concat([decipher.update(encryptedFileContent), decipher.final()]);
 
   return result;
 };
 
-const fileEncryptor = (sourceFile, destinationFile, privateKey, flag=null, callback) => {
-
-  if(fs.existsSync(destinationFile) && flag != 'r'){
-    callback(null, "the file already exists! if you want to overwrite it pass 'r' into flag parameter.")
-    return
+const fileEncryptor = (sourceFile, destinationFile, privateKey, flag = null, callback) => {
+  if (fs.existsSync(destinationFile) && flag != "r") {
+    callback(
+      null,
+      "the file already exists! if you want to overwrite it pass 'r' into flag parameter.",
+    );
+    return;
   }
 
   fs.readFile(sourceFile, (err, file) => {
     if (err) {
-      callback(null, err.message)
-      return
-    };
+      callback(null, err.message);
+      return;
+    }
 
     const encryptedFileContent = fileContent_encrypter(file, privateKey);
 
     fs.writeFile(destinationFile, encryptedFileContent, (err, file) => {
       if (err) {
-        callback(null, err.message)
-        return
-      };
+        callback(null, err.message);
+        return;
+      }
 
       callback("Encrypted Successfully!", null);
-      return
+      return;
     });
   });
 };
 
-const fileDecryptor = (filePath, destinationPath=null, privateKey, callback) => {
-
-  if(!fs.existsSync(filePath)){
-    callback(null, "No such a file in this giver path: " + filePath)
-    return
+const fileDecryptor = (filePath, destinationPath = null, privateKey, callback) => {
+  if (!fs.existsSync(filePath)) {
+    callback(null, "No such a file in this giver path: " + filePath);
+    return;
   }
 
   fs.readFile(filePath, (err, file) => {
     if (err) {
-      callback(null, err.message)
-      return
-    };
+      callback(null, err.message);
+      return;
+    }
     let decryptedFileContent = fileContent_decrypter(file, privateKey);
-    
 
-    if(destinationPath){
+    if (destinationPath) {
+      let file = filePath.split(".");
+      let extension = file[1];
 
-      let file = filePath.split('.')
-      let extension = file[1]
-
-      if(extension.toLowerCase() === 'json'){
-        decryptedFileContent = JSON.stringify(decryptedFileContent, null, 2)
+      if (extension.toLowerCase() === "json") {
+        decryptedFileContent = JSON.stringify(decryptedFileContent, null, 2);
       }
 
       fs.writeFile(destinationPath, decryptedFileContent, (err, file) => {
         if (err) {
-          callback(null, err.message)
-          return
-        };
-  
+          callback(null, err.message);
+          return;
+        }
+
         callback("Decrypted Successfully!", null);
-        return
+        return;
       });
-
-    }else{
-      callback(decryptedFileContent.toString('utf-8'), null);
-      return
+    } else {
+      callback(decryptedFileContent.toString("utf-8"), null);
+      return;
     }
-
   });
 };
 
@@ -114,7 +109,10 @@ const keyEncryptor = (key, privateKey) => {
     .update(String(privateKey))
     .digest("base64")
     .substring(0, 32);
-  return crypto_js.AES.encrypt(key, secretKey).toString();
+
+  let encJson = CryptoJS.AES.encrypt(JSON.stringify(key), secretKey).toString();
+  let encData = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(encJson));
+  return encData;
 };
 const keyDecryptor = (encrypted, privateKey) => {
   const secretKey = node_crypto
@@ -122,13 +120,22 @@ const keyDecryptor = (encrypted, privateKey) => {
     .update(String(privateKey))
     .digest("base64")
     .substring(0, 32);
-  var bytes = crypto_js.AES.decrypt(encrypted, secretKey);
-  return bytes.toString(crypto_js.enc.Utf8);
+
+  let decData = CryptoJS.enc.Base64.parse(encrypted).toString(CryptoJS.enc.Utf8);
+  let bytes = CryptoJS.AES.decrypt(decData, secretKey).toString(CryptoJS.enc.Utf8);
+  return JSON.parse(bytes);
+};
+
+const HashKey = (key, privateKey) => {
+  return Base64.stringify(CryptoJS.HmacSHA512(key, privateKey));
 };
 
 module.exports = {
   fileEncryptor,
   fileDecryptor,
+  fileContent_encrypter,
+  fileContent_decrypter,
   keyEncryptor,
   keyDecryptor,
+  HashKey,
 };
